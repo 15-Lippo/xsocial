ab1e2f9bebcdec72781f1d73ea9a3659dd6225b8
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-01-30 22:12:57 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+import './styles/styles.css';
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navigation from '../src/components/Navigation';
+
+const darkTheme = createTheme({
+  typography: {
+    fontFamily: 'monospace'
+  },
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Your identity across web3, one name for all your crypto addresses, and your decentralised website."
+        />
+        <title>Lisprocoin Registrar | Use Of Name for Anything Across Web3</title>
+      </Head>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+        <Navigation />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
